cd5563070a1c2fa86d4a0f42583947d0dfbe31e8
Bianca Leão <biancapeleao@gmail.com>
2025-05-09 11:45:32 -0300

    feat: removendo contando manobras

--- Estacionamento.java
+++ Estacionamento.java
@@ -4,6 +4,7 @@ public class Estacionamento {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Pilha estacionamento = new Pilha(10);
+        Pilha estacionamentoAuxiliar = new Pilha(10);
 
         System.out.println("Bem-vindo ao Estacionamento do Bololo Haha!");
         System.out.println("Digite '0' se deseja adicionar um carro ao estacionamento.");
@@ -19,12 +20,6 @@ public class Estacionamento {
                 break;
 
             while (partes.length != 2) {
-                System.out.println("   (o_o)   ");
-                System.out.println("  /| |\\   ");
-                System.out.println("  / \\ \\   ");
-                System.out.println(" (_____)  ");
-                System.out.println("  |   |   ");
-
 
                 System.out.println("BOLOLO HAHA BOLOLO HAHAHAHA");
                 System.out.println("Por favor, verifique se o formato da entrada está correto!");
@@ -47,12 +42,33 @@ public class Estacionamento {
                         System.out.println("Carro " + placa + " não entrou: estacionamento cheio.");
                     }
                 }    
+
+                if (tipoMovimento.equals("1")) {
+                    if (!estacionamento.contains(placa)) {
+                        System.out.println("Carro " + placa + " não está no estacionamento.");
+                    } else if (!estacionamento.isEmpty()) {
+                        int manobras = 0;
+                
+                        while (!estacionamento.isEmpty()) {
+                            String topo = estacionamento.pop();
+                            if (topo.equals(placa)) {
+                                System.out.println("Carro " + placa + " saiu do estacionamento com " + manobras + " manobras.");
+                                break;
+                            } else {
+                                estacionamentoAuxiliar.push(topo);
+                                manobras++;
+                            }
+                        }
+                
+                        while (!estacionamentoAuxiliar.isEmpty()) {
+                            estacionamento.push(estacionamentoAuxiliar.pop());
+                        }
+                    } else {
+                        System.out.println("Carro " + placa + " não saiu: estacionamento vazio.");
+                    }
+                }                
             }
-
-            // se o digitado for 1 remover ate chegar na placa digitada e ir contando as
-            // manobras
         }
-
         sc.close();
     }
 }
